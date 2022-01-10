b3d3ba352f048b33e8e425de3f6593cacbcefc8b
alicika <52126032+alicika@users.noreply.github.com>
2022-01-10 20:46:14 +0900

    Feat/set envvar (#3)
    * chore: fix URL to parse the given string correctly
    
    * add a header to the query
    
    * add dependency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use url::Url;
+use dotenv;
+use once_cell::sync::Lazy;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[allow(dead_code)]
@@ -24,14 +25,14 @@ async fn main() -> Result<(), reqwest::Error> {
         ),
     };
 
-    let url = Url::parse(&url)?;
-    //let token = std::env::var("TOKEN").unwrap();
-    //let token = format!("Bearer {}", token);
+    let token = std::env::var("TOKEN").unwrap_or("PSUEDO-TOKEN".to_string());
+    let token = format!("Bearer {}", token);
     //let client = reqwest::Client::new().get(&url).header("authorization", token);
     //println!("{:#?}", client);
 
     let resp = t
-        .get(&url)
+        .get(url)
+        .header("authorization", token)
         .send()
         .await?
         .json::<HashMap<String, String>>()
