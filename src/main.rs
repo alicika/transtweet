c506be23a9b496ed17ae9ccc7bd36015236c6144
alicika <52126032+alicika@users.noreply.github.com>
2022-01-20 23:51:42 +0900

    add a test template

--- src/main.rs
+++ src/main.rs
@@ -56,3 +56,14 @@ async fn extract_url(source: String) -> Result<Vec<String>> {
     //dbg!(urls)
     Ok(urls)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+            assert_eq!(2 + 2, 4);
+        }
+    #[test]
+    extract_url()
+}
